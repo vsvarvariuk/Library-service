434b251c8d7de60de91fb8bddbf46feee3d987c9
vavarvariuk <vasya.varvariuk.27@gmail.com>
2025-04-01 16:00:54 +0300

    create ManageUserApiView, add url

--- user/views.py
+++ user/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from user.serializers import UserSerializer
 
@@ -7,3 +7,11 @@ from user.serializers import UserSerializer
 class UserCreateApiView(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = (AllowAny,)
+
+
+class ManageUserApiView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
