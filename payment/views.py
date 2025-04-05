c52f4fa69418eedbe3f818cc12dc405cf4e2eb32
vavarvariuk <vasya.varvariuk.27@gmail.com>
2025-04-05 12:55:02 +0300

    add .env & .emv_sample

--- payment/views.py
+++ payment/views.py
@@ -1,13 +1,10 @@
 from rest_framework import generics
-
 from library_service import settings
-from django.shortcuts import render
-
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Payment
 import stripe
-import os
+
 
 from .serializers import PaymentSerializer, PaymentAdminSerializer
 
